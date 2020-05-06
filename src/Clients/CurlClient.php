70b90316590724c0f84fea804df955c8fb0dac00
Willy Bernardes Maciel <willy_maciel@live.com>
2020-05-06 09:23:07 -0300

    Alterações cliente curl

--- src/Clients/CurlClient.php
+++ src/Clients/CurlClient.php
@@ -14,40 +14,51 @@ class CurlClient implements Client
 {
     CONST USER_AGENT = 'SWServiceInvoker';
     private $baseUri;
-    private $headers;
+    private $headers = [];
     private $headersProcessed;
     private $cookies;
+    private $query = [];
+    private $queryProcessed;
 
     function __construct($baseUri)
     {
         $this->baseUri = $baseUri;
 
         $this->headers = [
-            "Cache-Control: no-cache",
-            "Content-Type: text/xml;charset=ISO-8859-1"
+            'Cache-Control' => 'no-cache',
+            'Content-Type'  => 'text/xml;charset=ISO-8859-1'
         ];
     }
 
-    public function get($endpoint, $body = [])
+    public function get($endpoint, $params = [])
     {
-        return $this->request('GET', $endpoint, $body);
+        return $this->request('GET', $endpoint, $params);
     }
 
-    public function post($endpoint, $body = [])
+    public function post($endpoint, $params = [])
     {
-        return $this->request('GET', $endpoint, $body);
+        return $this->request('POST', $endpoint, $params);
     }
 
-    private function request($method = 'GET', $endpoint, $body = NULL)
+    private function request($method = 'GET', $endpoint, $params = NULL)
     {
         $curl = curl_init();
 
+        $body = (isset($params['body'])) ? $params['body'] : NULL;
+        $body = isoToUtf8($body);
         $body = (is_array($body)) ? http_build_query($body) : $body;
-        $this->processHeaders();
+
+        $localQuery = (isset($params['query'])) ? $params['query'] : [];
+        $localHeaders = (isset($params['headers'])) ? $parmas['headers'] : [];
+
+        $this->processHeaders($localHeaders);
+        $this->processQuery($localQuery);
+
+        $url = $this->baseUri . $endpoint . $this->queryProcessed;
 
         $options = [
             CURLOPT_USERAGENT => STATIC::USER_AGENT,
-            CURLOPT_URL => $this->baseUri . $endpoint,
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_ENCODING => "",
@@ -87,14 +98,54 @@ class CurlClient implements Client
         }
     }
 
-    private function processHeaders()
+    private function processHeaders(array $localHeaders = [])
     {
+        if(!is_array($localHeaders))
+        {
+            throw new \Exception("Propriedade headers deve ser do tipo Array ['key' => value]", 1);
+        }
         $this->headersProcessed = [];
-        foreach($this->headers as $key => $header)
+        $headers = array_merge($this->headers, $localHeaders);
+
+        foreach($headers as $key => $header)
         {
             $this->headersProcessed[] = "$key: $header";
         }
     }
+
+    public function addQuery(array $queries)
+    {
+        foreach($queries as $key => $value)
+        {
+            $this->query[$key] = $value;
+        }
+    }
+
+    private function processQuery($query = [])
+    {
+        if(!is_array($query))
+        {
+            throw new \Exception("Propriedade Query deve ser do tipo Array ['key' => value]", 1);
+        }
+
+        $this->queryProcessed = [];
+        $query = array_merge($this->query, $query);
+
+        if(empty($query))
+        {
+            return;
+        }
+
+        $array = [];
+
+        foreach ($query as $key => $value)
+        {
+            $array[] = "$key=$value";
+        }
+
+        $this->queryProcessed = '?' . implode('&', $array);
+    }
+
 
     private function convertResponse($response)
     {
